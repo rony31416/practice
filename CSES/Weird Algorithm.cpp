ab5866563acc35916eb8881a4157b96fd4e542b8
rony31416 <ronymajumder1171@gmail.com>
2023-01-22 16:32:29 +0600

    solved

--- CSES/Weird Algorithm.cpp
+++ CSES/Weird Algorithm.cpp
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    long long n;
+    scanf("%lld",&n);
+    while(n != 1)
+    {
+            printf("%lld ",n);
+            if(n%2 == 0)n/=2;
+            else n = 3*n+1;
+    }
+    
+    printf("1\n");
+    return 0;
+}
 
