873dbc00f0f1f38980afb27e2b71bedfbf4285dc
rony31416 <ronymajumder1171@gmail.com>
2023-01-28 19:50:12 +0600

    update

--- Numerical Analysis/Root find using Bisection Method.c
+++ Numerical Analysis/Root find using Bisection Method.c
@@ -9,9 +9,9 @@
 int main()
 {
     int i;
-    double a = 1.25;
-    double b = 1.35;
-    double TOL = 1.0e-5;
+    double a = 1;
+    double b = 2;
+    double TOL = 0.00005;
     double x0 ;
     double fa,fb,fx0;
     fa = F(a);
@@ -23,9 +23,10 @@ int main()
     }
     for(int i = 1 ; i< MAXN ; i++)
     {
+
         x0 = (a+b)/2;
         fx0 = F(x0);
-
+         printf("%d\t%lf\t%lf\t%lf\t%lf\n",i,a,b,x0,fx0);
         if(fabs(fx0) <= TOL)
         {
             printf("Root : %lf, Itr : %d\n",x0,i);
