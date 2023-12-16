90c94e42f46b83f71467df45dee69f18a0cac865
rony31416 <ronymajumder1171@gmail.com>
2023-12-17 02:03:36 +0600

    nice one

---
+++ LeetCode/189. Rotate Array.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/rotate-array/description/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k%=n;
+        vector<int> v;
+        for(int  i = n-k ; i < n ; i++ ) v.push_back(nums[i]);
+        for(int i = 0 ; i < n-k ; i++) v.push_back(nums[i]);
+
+        nums = v;
+        
+        
+    }
+};
