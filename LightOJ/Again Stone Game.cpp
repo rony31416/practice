1e8ba9e5550267e2166cf62c1fdcaae37ba15139
rony31416 <ronymajumder1171@gmail.com>
2024-01-01 01:57:33 +0600

    mofidied

--- LightOJ/Again Stone Game.cpp
+++ LightOJ/Again Stone Game.cpp
@@ -1,10 +1,8 @@
-
+//https://lightoj.com/problem/again-stone-game
 #include<bits/stdc++.h>
 using namespace std;
 
 typedef  long long int ll ;
-
-
 int grundy_(int n)
 {
     if(n<2) return 0;
@@ -15,12 +13,10 @@ int grundy_(int n)
 
 }
 
+
 int main()
 {
-    //memset(grundy,-1,sizeof(grundy));
-
-    //find_grundy_number(10003);
-    //for(int i = 1 ; i< 100 ; i++)cout<<i<<" "<<grundy[i]<<nl;
+    
     int t;
     cin>>t;
     for(int i = 1 ; i<= t; i++)
